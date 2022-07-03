0c544dc9bf80d6c6f7ed8ca1608d9cb2e34402e3
Ahmed Hajeyah <hajeyah@gmail.com>
2022-07-03 19:28:38 +0300

    done

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,17 @@
 def main():
 	# write your code here
+	number=input('Enter a number from 1 to 12: ')
+	noun=input('Enter a noun (plural): ')
+	name=input('Enter a name: ')
+	sentence=input('Enter any sentence: ')
+	verb=input('Enter a verb: ')
 
-
-
+	print("It was "+number+" o'clock when I heard a knock at the door.")
+	print("I opened the door and there was a box full of "+noun+" with a note saying "+name+".")
+	print("Just as I closed the door I heard a scream "+sentence+".")
+	print("I froze in place and all I could do was "+verb+".'")
+		
 if __name__ == '__main__':
-	main()
+	main(
+	
+	)
